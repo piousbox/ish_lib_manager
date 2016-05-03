3b742843e3545e699d4af963d5f02a62cab125d0
Victor Piousbox <piousbox@gmail.com>
2016-05-02 22:34:15 -0700

    layout at least works

--- app/controllers/ish_lib_manager/welcome_controller.rb
+++ app/controllers/ish_lib_manager/welcome_controller.rb
@@ -4,6 +4,9 @@ module IshLibManager
   class WelcomeController < IshLibManager::ApplicationController
 
     def home
+      @page_title = 'Welcome home!'
+      
+      render :action => 'empty', :layout => 'ish_lib_manager/application'
     end
     
   end
